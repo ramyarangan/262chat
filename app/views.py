94f2b85226769ff72b37ebef8b4e2845caabdcc5
Michelle Deng <mdeng@college.harvard.edu>
2016-02-26 18:53:45 -0500

    add boilerplate server

---
+++ app/views.py
@@ -0,0 +1,13 @@
+
+'''
+The views are the handlers that respond to requests from web browsers or other 
+clients. Each view function is mapped to one or more request URLs.
+'''
+
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+    
