128d098831fe9137f230b073d2ca984fa8a122c8
Michelle Deng <mdeng@college.harvard.edu>
2016-02-29 12:41:18 -0500

    db migrations for group messaging

--- app/models.py
+++ app/models.py
@@ -1,6 +1,20 @@
 from app import db
 
 
+class Seen(db.Model):
+    '''
+    last seen message id, per user per group
+    '''
+
+    __tablename__ = 'seen'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    viewer = db.Column(db.String(20), db.ForeignKey('user.username'))
+    groupname = db.Column(db.String(20), db.ForeignKey('group.groupname'))
+    last_seen_id = db.Column(db.Integer, db.ForeignKey('message.id'), default=0)
+
+
 class Message(db.Model):
     __tablename__ = 'message'
 
@@ -14,9 +28,11 @@ class Message(db.Model):
     received = db.Column(db.Integer, default=STATUS_NEW)
 
     sender_username = db.Column(db.String(20), db.ForeignKey('user.username'), \
-    	nullable=False)
+        nullable=False)
     to_username = db.Column(db.String(20), db.ForeignKey('user.username'), \
-    	nullable=False)
+        nullable=False)
+    to_groupname = db.Column(db.String(40), db.ForeignKey('group.groupname'), \
+        nullable=False)
 
     def __repr__(self):
         return '<Message %r>' % (self.body)
@@ -33,28 +49,30 @@ class User(db.Model):
     username = db.Column(db.String(20), index=True, unique=True)
 
     received_msgs = db.relationship('Message', backref='to', lazy='dynamic', \
-    	foreign_keys=[Message.to_username])
+        foreign_keys=[Message.to_username])
     sent_msgs = db.relationship('Message', lazy='dynamic', \
-    	foreign_keys=[Message.sender_username])
+        foreign_keys=[Message.sender_username])
 
     groups = db.relationship("Group", \
-    	secondary=users_groups_assoc_table, \
-    	back_populates="users")
+        secondary=users_groups_assoc_table, \
+        back_populates="users")
 
     def __repr__(self):
         return '<User %r>' % (self.username)
 
 class Group(db.Model):
-	__tablename__ = 'group'
+    __tablename__ = 'group'
 
-	id = db.Column(db.Integer, primary_key=True)
-	groupname = db.Column(db.String(40), index=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    groupname = db.Column(db.String(40), index=True, unique=True)
+    
+    msgs = db.relationship('Message', backref='to', lazy='dynamic', foreign_keys=[Message.to_groupname])
+    
+    users = db.relationship("User", secondary=users_groups_assoc_table, \
+        back_populates="groups")
 
-	users = db.relationship("User", secondary=users_groups_assoc_table, \
-		back_populates="groups")
-
-	def __repr__(self):
-		return '<Group %r>' % (self.groupname)
+    def __repr__(self):
+        return '<Group %r>' % (self.groupname)
 
 
 
